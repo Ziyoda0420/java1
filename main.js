586c570dee5a75817f86e51b1aa306f5e0365e29
Ziyoda0420 <mirzakirovaziyoda@gmail.com>
2025-01-05 19:41:35 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let userName = prompt('введите своё имя')
+let userLastname = prompt('введите свою фамилию')
+let userNumber = prompt('введите свой номер')
+let userAge = prompt('введите свой возрост')
+let userAdress = prompt('введите свой адрес')
+
+
+
+
+console.log('ваше имя' + userName + '\n ваше фамилия' + userLastname +'\n свой номер'+userNumber+'\n свой возрост'+userAge
++'\n свой адрес'+userAdress
+);
